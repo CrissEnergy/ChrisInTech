6ec11f7313eeb6770e7e4fb3890891e09952cc85
Criss Energy <crissenergy1@gmail.com>
2025-12-27 18:51:39 +0000

    Try fixing this error: `Console Error: FirebaseError: Firebase Storage:

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -2,7 +2,8 @@ export const firebaseConfig = {
   "projectId": "studio-7746671732-245ea",
   "appId": "1:371373330488:web:6ca59edd480bd2a52156cd",
   "apiKey": "AIzaSyAAsQMTgrZAMpHblbNzyNCtrjo-f_7QNMk",
-  "authDomain": "studio-7746671732-245ea.firebaseapp.com",
+  "authDomain": "studio-774ge671732-245ea.firebaseapp.com",
+  "storageBucket": "studio-7746671732-245ea.appspot.com",
   "measurementId": "",
   "messagingSenderId": "371373330488"
 };
