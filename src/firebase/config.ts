b47cd6d87a3384374dde42c33c5cf36ce5ffe683
Criss Energy <crissenergy1@gmail.com>
2025-12-27 18:02:28 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7746671732-245ea",
+  "appId": "1:371373330488:web:6ca59edd480bd2a52156cd",
+  "apiKey": "AIzaSyAAsQMTgrZAMpHblbNzyNCtrjo-f_7QNMk",
+  "authDomain": "studio-7746671732-245ea.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "371373330488"
+};
