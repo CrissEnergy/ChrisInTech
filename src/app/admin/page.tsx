ec3ef9f917a176818fa1697374f7654fa8e97eec
Criss Energy <crissenergy1@gmail.com>
2025-12-29 21:07:57 +0000

    blend logo background to match color header , i dont want the white back

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -533,7 +533,7 @@ export default function AdminDashboard() {
         </div>
       </header>
        <div className="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8 lg:grid-cols-3 xl:grid-cols-3">
-        <div className="grid auto-rows-max items-start gap-4 md:gap-8 lg:col-span-4">
+        <div className="grid auto-rows-max items-start gap-4 md:gap-8 lg:col-span-2">
           <div className="grid gap-4 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-2 xl:grid-cols-4">
             <Card className="sm:col-span-2">
               <CardHeader className="pb-3">
@@ -1111,5 +1111,7 @@ export default function AdminDashboard() {
     </>
   );
 }
+
+    
 
     
